d54edacbb66f4da094e06e1bb1fc80d908e37529
Matvey Levinson <matvey@levinson.pro>
2022-12-22 23:26:38 +0300

    feat(Project): show budget on condition

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -6,6 +6,7 @@ use App\Models\Profile;
 use App\Models\Project;
 use App\Traits\Moderation\Status;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectResource extends JsonResource
 {
@@ -17,8 +18,11 @@ class ProjectResource extends JsonResource
      */
     public function toArray($request)
     {
+        $user = Auth::user();
         $moderation = \Arr::get($this->moderationStatus, '0');
-        $canEdit = (string)$this->owner_id === (string)\Auth::user()?->profile?->id;
+        $isOwner = (string)$this->owner_id === (string)$user?->profile?->id;
+        $isPrivileged = in_array($user->role, ['admin', 'moderator']);
+        $canViewBudget = $isOwner || $isPrivileged;
         return [
             'id' => $this->id,
             'title' => $this->title,
@@ -32,8 +36,10 @@ class ProjectResource extends JsonResource
             'synopsis' => $this->synopsis,
             'relevance' => $this->relevance,
             'additional' => $this->resource->additional,
-            'budget' => $this->budget,
-            'co_financing' => $this->co_financing,
+
+            'budget' => $this->when($canViewBudget, $this->budget),
+            'co_financing' => $this->when($canViewBudget, $this->co_financing),
+
             'custom_members' => $this->custom_members,
             'audio_reference' => $this->audio_reference,
             'members' => ProfileMemberResource::collection($this->memberInvites),
@@ -46,11 +52,14 @@ class ProjectResource extends JsonResource
             'makeup' => new MediaResource($this->getFirstMedia(Project::MAKEUP_MEDIA)),
             'decorations' => new MediaResource($this->getFirstMedia(Project::DECORATIONS_MEDIA)),
             'location_reference' => new MediaResource($this->getFirstMedia(Project::LOCATIONS_MEDIA)),
-            'financial_plan' => new MediaResource($this->getFirstMedia(Project::FINANCIAL_PLAN_MEDIA)),
-            'financial_proof' => new MediaResource($this->getFirstMedia(Project::FINANCIAL_PROOF_MEDIA)),
+
+            'financial_plan' => $this->when($canViewBudget, new MediaResource($this->getFirstMedia(Project::FINANCIAL_PLAN_MEDIA))),
+            'financial_proof' => $this->when($canViewBudget, new MediaResource($this->getFirstMedia(Project::FINANCIAL_PROOF_MEDIA))),
+
             'partnership_proof' => MediaResource::collection($this->getMedia(Project::PARTNERSHIP_PROOF_MEDIA)),
 
-            'canEdit' => $canEdit,
+            'isOwner' => $isOwner,
+            'canEdit' => $isOwner,
 
             'moderation' => $this->when($this->status === Status::PENDING || $this->status === Status::REJECTED && isset($moderation), [
                 'startedAt' => $this->updatedAt,
