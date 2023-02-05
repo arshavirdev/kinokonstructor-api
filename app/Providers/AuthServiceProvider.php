8907dbf6360951b8bfdf0059603c573dc0524f33
Matvey Levinson <matvey@levinson.pro>
2023-02-05 20:32:27 +0100

    fix(ResetPassword): use proper mail

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,7 @@ use Illuminate\Auth\Notifications\VerifyEmail;
 use App\Mail\VerifyEmail as CustomVerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Auth\Notifications\ResetPassword;
-use App\Mail\ProjectInvitation as CustomResetPassword;
+use App\Mail\ResetPassword as CustomResetPassword;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Hash;
