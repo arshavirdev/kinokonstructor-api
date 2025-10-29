3299b3b17113be55d9f53efeb422559b099dbcd4
arshavirdev <arshavir.dev@gmail.com>
2025-10-29 16:03:48 +0400

    change name to names in requests table

--- database/migrations/2025_10_29_084916_add_names_to_requests_table.php
+++ database/migrations/2025_10_29_084916_add_names_to_requests_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('requests', function (Blueprint $table) {
+            $table->dropColumn('name');
             $table->jsonb('names')->default(value: '[]');
         });
     }
