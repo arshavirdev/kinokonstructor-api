61f0bae2f87469c7a0aa664e51e9e975f19274f2
arshavirdev <arshavir.dev@gmail.com>
2026-03-23 16:36:01 +0400

    fix to receive empty occupation_ids

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -33,7 +33,7 @@ class UpdateProfileRequest extends FormRequest
             ],
             'city' => 'nullable|string',
             'birthday' => 'nullable|date',
-            'occupation_id' => 'array|min:1',
+            'occupation_ids' => 'array',
             'occupation_ids.*' => "exists:occupations,id",
             'member_id' => 'nullable|string',
             "additional_information" => 'string|nullable',
